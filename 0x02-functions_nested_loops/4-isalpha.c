030f41889aaeb46b7ba4d531c2229e1f25f7ae74
MARTIN <69876670+Tylelol@users.noreply.github.com>
2022-07-05 13:29:08 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - Check main
+ * _isalpha - Check character
  * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
